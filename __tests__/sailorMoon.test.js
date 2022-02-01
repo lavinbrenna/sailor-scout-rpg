dba4873601ae4c67d2d4c88c9aa8f005522ae066
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-31 22:16:00 -0800

    add ability tests

--- __tests__/sailorMoon.test.js
+++ __tests__/sailorMoon.test.js
@@ -9,4 +9,16 @@ describe('SailorMoon', () => {
     expect(sailorMoon.tagline).toEqual("In the name of the moon, I'll punish you.");
     expect(sailorMoon.hitPoints).toEqual(100);
   });
+  test('should return 5 damage.', ()=>{
+    const sailorMoon = new SailorMoon();
+    expect(sailorMoon.moonTiaraMagic()).toEqual(5);
+  });
+  test('should return 10 damage', ()=>{
+    const sailorMoon = new SailorMoon();
+    expect(sailorMoon.moonHealingActivation()).toEqual(10);
+  });
+  test('should return 20 damage and "critical" message', () => {
+    const sailorMoon = new SailorMoon();
+    expect(sailorMoon.cosmicMoonPower()).toEqual("critical 20 damage");
+  });
 });
--- src/sailor-moon.js
+++ src/sailor-moon.js
@@ -4,3 +4,18 @@ export default function SailorMoon(){
   this.tagline = "In the name of the moon, I'll punish you.";
   this.hitPoints = 100;
 }
+
+SailorMoon.prototype.moonTiaraMagic = function (){
+  this.damage = 5;
+  return this.damage;
+};
+
+SailorMoon.prototype.moonHealingActivation = function (){
+  this.damage = 10;
+  return this.damage;
+};
+
+SailorMoon.prototype.cosmicMoonPower = function (){
+  this.damage = 20;
+  return "critical " + this.damage + " damage";
+};
